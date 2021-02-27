5ae38f9d5d35cbd348d7d249d2c2b342bc8260fa
mikeday37 <mikeday370146@gmail.com>
2021-02-27 12:36:57 -0500

    day 1 part 1, latest cs version

---
+++ AdventOfCode2016/Days/Common.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2016.Days
+{
+	/// <summary>
+	/// Common extension methods used by multiple days.
+	/// </summary>
+	public static class CommonExtensions
+	{
+		/// <summary>
+		/// Returns an enumeration of single-line strings, by default omitting blank lines.
+		/// </summary>
+		public static IEnumerable<string> ToLines(this string input, bool omitBlanks = true)
+		{
+			foreach (var rawLine in input.Split('\n'))
+			{
+				var line = rawLine.TrimEnd('\r');
+				if (omitBlanks && string.IsNullOrWhiteSpace(line))
+					continue;
+				yield return line;
+			}
+		}
+	}
+}
--- AdventOfCode2016/Days/Day01.cs
+++ AdventOfCode2016/Days/Day01.cs
@@ -10,9 +10,42 @@ namespace AdventOfCode2016.Days
 	[Challenge(01, "No Time for a Taxicab")]
 	class Day01 : ChallengeBase
 	{
+		enum Direction
+		{
+			Left, Right
+		}
+
+		class Instruction
+		{
+			public Direction Direction {get;set;}
+			public int Distance {get;set;}
+		}
+
+		IEnumerable<Instruction> ParseInstructions(string input)
+		{
+			foreach (var raw in input.Trim().Split(", "))
+				yield return new Instruction{
+					Direction = raw[0] == 'L' ? Direction.Left : Direction.Right,
+					Distance = int.Parse(raw[1..])
+				};
+		}
+
 		public override object Part1(string input)
 		{
-			return -1;
+			int x = 0, y = 0, dx = 0, dy = -1;
+
+			foreach (var i in ParseInstructions(input))
+			{
+				if (i.Direction == Direction.Left)
+					(dx, dy) = (dy, -dx);
+				else
+					(dx, dy) = (-dy, dx);
+
+				x += dx * i.Distance;
+				y += dy * i.Distance;
+			}
+
+			return x + y;
 		}
 
 		public override object Part2(string input)
