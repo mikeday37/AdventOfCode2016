03151f4f7485b5f7ef7d2d6d2dd0ec3891ccd30c
mikeday37 <mikeday370146@gmail.com>
2021-02-27 12:53:13 -0500

    day 1 both parts, restricted to C# version 6

--- AdventOfCode2016/Days/Day01.cs
+++ AdventOfCode2016/Days/Day01.cs
@@ -26,31 +26,70 @@ namespace AdventOfCode2016.Days
 			foreach (var raw in input.Trim().Split(", "))
 				yield return new Instruction{
 					Direction = raw[0] == 'L' ? Direction.Left : Direction.Right,
-					Distance = int.Parse(raw[1..])
+					Distance = int.Parse(raw.Substring(1))
 				};
 		}
 
-		public override object Part1(string input)
+		struct Location
+		{
+			public int X, Y;
+			public override int GetHashCode()
+			{
+				return HashCode.Combine(X, Y);
+			}
+			public override bool Equals(object obj)
+			{
+				return obj is Location && Equals((Location) obj);
+			}
+			public bool Equals(Location l)
+			{
+				return X == l.X && Y == l.Y;
+			}
+		}
+		
+		IEnumerable<Location> FollowInstructions(string input)
 		{
 			int x = 0, y = 0, dx = 0, dy = -1;
 
 			foreach (var i in ParseInstructions(input))
 			{
 				if (i.Direction == Direction.Left)
-					(dx, dy) = (dy, -dx);
+				{
+					var oldDx = dx;
+					dx = dy;
+					dy = -oldDx;
+				}
 				else
-					(dx, dy) = (-dy, dx);
+				{
+					var oldDx = dx;
+					dx = -dy;
+					dy = oldDx;
+				}
 
-				x += dx * i.Distance;
-				y += dy * i.Distance;
+				for (int n = 1; n <= i.Distance; n++)
+				{
+					x += dx;
+					y += dy;
+					yield return new Location{X = x, Y = y};
+				}
 			}
+		}
 
-			return x + y;
+		public override object Part1(string input)
+		{
+			var end = FollowInstructions(input).Last();
+			return end.X + end.Y;
 		}
 
 		public override object Part2(string input)
 		{
-			return -1;
+			HashSet<Location> locations = new HashSet<Location>();
+			foreach (var loc in FollowInstructions(input))
+				if (locations.Contains(loc))
+					return loc.X + loc.Y;
+				else
+					locations.Add(loc);
+			return "no location is visited twice";
 		}
 	}
 }
